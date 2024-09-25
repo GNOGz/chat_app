0998cb731b3ce6876a3f3c3d60e520eaab2cbd8a
GNOGz <99389733+GNOGz@users.noreply.github.com>
2024-09-25 21:25:21 +0700

    text black

--- src/app/component/chatbubble.tsx
+++ src/app/component/chatbubble.tsx
@@ -4,7 +4,7 @@ export default function Chatbubble(prop:bubbleContent) {
   const pos:string = prop.right?" text-right": " ";
   return (
     <div className={"py-8 " + pos}>
-    <span className={ ("border rounded-md border-solid p-3 m-5 ") 
+    <span className={ ("border rounded-md border-solid p-3 m-5 text-black ") 
       + (prop.right?" border-indigo-600 bg-[#8b91da]":"border-black bg-[#cfd1e9]")
     }>
       {prop.text}
